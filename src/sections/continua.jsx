be7c822728ee8fa8260e077f71782776a19a78c1
Valeria <vdelaconcep@gmail.com>
2025-11-29 09:52:41 -0300

    Animación pasada a css puro

--- src/sections/continua.jsx
+++ src/sections/continua.jsx
@@ -4,33 +4,33 @@ const AnimacionContinua = () => {
     return (
         <div className='min-h-dvh w-full flex justify-center items-center overflow-hidden relative'>
             <article
-                className={`h-0 w-0 border-b-20 border-b-red-700 border-l-12 border-l-transparent border-r-12 border-r-transparent growing absolute bottom-1/2 left-1/2 -translate-x-1/2`}>
-
-            </article >
-            <article 
-            style={{animationDelay:'1s'}}
-            className={`h-0 w-0 border-b-20 border-b-black border-l-12 border-l-transparent border-r-12 border-r-transparent growing absolute bottom-1/2 left-1/2 -translate-x-1/2`}>
-
+                style={{borderBottomColor: '#dc2626'}}
+                className='center_triangle growing'>
             </article>
+            
             <article
-                style={{ animationDelay: '2s' }}
-                className={`h-0 w-0 border-b-20 border-b-white border-l-12 border-l-transparent border-r-12 border-r-transparent growing absolute bottom-1/2 left-1/2 -translate-x-1/2`}>
-
+                style={{borderBottomColor: '#000000', animationDelay:'1s'}}
+                className='center_triangle growing'>
             </article>
-            <article
-                style={{ animationDelay: '3s' }}
-                className={`h-0 w-0 border-b-20 border-b-red-700 border-l-12 border-l-transparent border-r-12 border-r-transparent growing absolute bottom-1/2 left-1/2 -translate-x-1/2`}>
 
-            </article >
             <article
-                style={{ animationDelay: '4s' }}
-                className={`h-0 w-0 border-b-20 border-b-black border-l-12 border-l-transparent border-r-12 border-r-transparent growing absolute bottom-1/2 left-1/2 -translate-x-1/2`}>
-
+                style={{ borderBottomColor: '#FFFFFF', animationDelay: '2s' }}
+                className='center_triangle growing'>
             </article>
-            <article
-                style={{ animationDelay: '5s' }}
-                className={`h-0 w-0 border-b-20 border-b-white border-l-12 border-l-transparent border-r-12 border-r-transparent growing absolute bottom-1/2 left-1/2 -translate-x-1/2`}>
 
+            <article
+                style={{ borderBottomColor: '#dc2626', animationDelay: '3s' }}
+                className='center_triangle growing'>
+            </article>
+
+            <article
+                style={{ borderBottomColor: '#000000', animationDelay: '4s' }}
+                className='center_triangle growing'>
+            </article>
+
+            <article
+                style={{ borderBottomColor: '#FFFFFF', animationDelay: '5s' }}
+                className='center_triangle growing'>
             </article>
             
         </div>
